4760eb8b10ae473125e82a8ae9138dfe5fcc54da
TorresCabral <iohanaatc@gmail.com>
2022-04-17 14:15:33 -0300

    3 telas sendo desenhadas

--- index.js
+++ index.js
@@ -1,35 +1,60 @@
-console.log('[DevSoutinho] Flappy Bird');
+console.log('[Iohana] Drone 2D');
 
-const sprites = new Image();
-sprites.src = './sprites.png';
+const ufu = new Image();
+ufu.src = './ufu.png';
+
+const drone = new Image();
+drone.src = './drone.png';
+
+const start = new Image();
+start.src = './start.png';
 
 const canvas = document.querySelector('canvas');
 const contexto = canvas.getContext('2d');
 
 
-// [Plano de Fundo]
+// [Tela de Início]
+const telaInicio = {
+  startX: 0,
+  startY: 0,
+  largura: 1476,
+  altura: 638,
+  x: (canvas.width/2),
+  y: (canvas.height/2),
+  desenha() {
+    contexto.drawImage(
+      start,
+      telaInicio.startX, telaInicio.startY,
+      telaInicio.largura, telaInicio.altura,
+      telaInicio.x, telaInicio.y,
+      telaInicio.largura, telaInicio.altura,
+    );    
+  },
+};
+
 const planoDeFundo = {
-  spriteX: 390,
-  spriteY: 0,
-  largura: 275,
-  altura: 204,
+  ufuX: 0,
+  ufuY: 0,
+  largura: 1475,
+  altura: 627,
   x: 0,
-  y: canvas.height - 204,
+  y: 0,
+  //y: canvas.height - 204,
   desenha() {
     contexto.fillStyle = '#70c5ce';
     contexto.fillRect(0,0, canvas.width, canvas.height)
 
     contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      ufu,
+      planoDeFundo.ufuX, planoDeFundo.ufuY,
       planoDeFundo.largura, planoDeFundo.altura,
       planoDeFundo.x, planoDeFundo.y,
       planoDeFundo.largura, planoDeFundo.altura,
     );
 
     contexto.drawImage(
-      sprites,
-      planoDeFundo.spriteX, planoDeFundo.spriteY,
+      ufu,
+      planoDeFundo.ufuX, planoDeFundo.ufuY,
       planoDeFundo.largura, planoDeFundo.altura,
       (planoDeFundo.x + planoDeFundo.largura), planoDeFundo.y,
       planoDeFundo.largura, planoDeFundo.altura,
@@ -37,59 +62,82 @@ const planoDeFundo = {
   },
 };
 
-// [Chao]
-const chao = {
-  spriteX: 0,
-  spriteY: 610,
-  largura: 224,
-  altura: 112,
-  x: 0,
-  y: canvas.height - 112,
+const drone2D = {
+  droneX: 0,
+  droneY: 0,
+  largura: 30,
+  altura: 30,
+  x: 10,
+  y: 50,
+  gravidade: 0.25,
+  velocidade: 0,
+  
+  atualiza(){
+    drone2D.velocidade = drone2D.velocidade + drone2D.gravidade;
+    drone2D.y = drone2D.y + drone2D.velocidade;
+  },
   desenha() {
     contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      chao.x, chao.y,
-      chao.largura, chao.altura,
+      drone,
+      drone2D.droneX, drone2D.droneY, // Sprite X, Sprite Y
+      drone2D.largura, drone2D.altura, // Tamanho do recorte na sprite
+      drone2D.x, drone2D.y,
+      drone2D.largura, drone2D.altura,
     );
+  }
+};
 
-    contexto.drawImage(
-      sprites,
-      chao.spriteX, chao.spriteY,
-      chao.largura, chao.altura,
-      (chao.x + chao.largura), chao.y,
-      chao.largura, chao.altura,
-    );
+////////TELAS
+
+let telaAtiva = {};
+function mudaTela(novaTela) {
+  telaAtiva = novaTela
+};
+
+const telas = {
+  inicio: {
+    desenha() {
+      planoDeFundo.desenha();
+      drone2D.desenha();
+      telaInicio.desenha();
+    },
+    click() {
+      mudaTela(telasJogo);
+    },
+    atualiza() {
+
+    },
   },
 };
 
-const flappyBird = {
-  spriteX: 0,
-  spriteY: 0,
-  largura: 33,
-  altura: 24,
-  x: 10,
-  y: 50,
+telasJogo = {
   desenha() {
-    contexto.drawImage(
-      sprites,
-      flappyBird.spriteX, flappyBird.spriteY, // Sprite X, Sprite Y
-      flappyBird.largura, flappyBird.altura, // Tamanho do recorte na sprite
-      flappyBird.x, flappyBird.y,
-      flappyBird.largura, flappyBird.altura,
-    );
+    planoDeFundo.desenha();
+    drone2D.desenha();
+  },
+  atualiza() {
+    drone2D.atualiza();
   }
-}
+};
 
 function loop() {
+  /*drone2D.atualiza();
   planoDeFundo.desenha();
-  chao.desenha();
-  flappyBird.desenha();
+  drone2D.desenha(); 
+  telaInicio.desenha();*/
 
-  flappyBird.y = flappyBird.y + 1;
+  telaAtiva.desenha();
+  telaAtiva.atualiza();
 
   requestAnimationFrame(loop);
+
 }
 
+window.addEventListener('click', function() {
+  if(telaAtiva.click) {
+    telaAtiva.click();
+  }
+});
+
+mudaTela(telas.inicio);
 loop();
